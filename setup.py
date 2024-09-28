a4fccecc1c9173d91e21dd2c9f7952d93460284f
Sergey Mikhno <sergey.mikhno@galenica.com>
2024-09-28 13:59:43 +0200

    setup

--- setup.py
+++ setup.py
@@ -1,32 +1,10 @@
-import setuptools
-import sys
-import os
+from distutils.core import setup, Extension
 import numpy as np
 
-rlsa_fast_ext = setuptools.Extension("pythonRLSA.rlsa_fast", sources = ["rlsa_fast/rlsa_extension.c"], include_dirs = [np.get_include()])
+rlsa_fast_ext = Extension("rlsa_fast", sources = ["rlsa_fast/rlsa_extension.c"], include_dirs = [np.get_include()])
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-setuptools.setup(
-    name="pythonRLSA",
-    version="1.0.0",
-    author="VasistaReddy",
-    author_email="vasista.1245@gmail.com",
-    description="Python Run Length Smoothing Algorithm for Document Processing",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=setuptools.find_packages(),
+setup(
+    name="rlsa_fast",
+    version="0.0.1",
     ext_modules = [rlsa_fast_ext],
-    install_requires=['numpy'],
-    url="https://github.com/Vasistareddy/pythonRLSA",
-    download_url='https://github.com/Vasistareddy/pythonRLSA',
-    include_package_data=True,
-    zip_safe=False,
-    keywords=['opencv', 'rlsa', 'image', 'documents', 'article', 'OCR', 'extraction'],
-    classifiers=[
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Scientific/Engineering :: Image Recognition',
-        'License :: OSI Approved :: MIT License',
-        'Topic :: Software Development :: Version Control :: Git',
-    ],
 )
