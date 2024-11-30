04544a2fa2d55c88b2a646ba358414d23c94d6d0
Sergey Mikhno <sergey.mikhno@galenica.com>
2024-11-30 10:46:43 +0100

    setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup, Extension
 import numpy as np
 
-rlsa_fast_ext = Extension("rlsa_fast", sources = ["rlsa_fast/rlsa_extension.c"], include_dirs = [np.get_include()])
+rlsa_fast_ext = Extension("rlsafast", sources = ["rlsa_fast/rlsa_extension.c"], include_dirs = [np.get_include()])
 
 setup(
-    name="rlsa_fast",
-    version="0.0.1",
+    name="rlsafast",
+    version="0.0.2",
     ext_modules = [rlsa_fast_ext],
 )
